52af08d841274920f3f08c8cd4a6151a48ff776e
Pedro Ferreira <peferreira98@outlook.com>
2020-05-17 22:09:04 +0100

    Delete config.js

--- ARQSI_IT2/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'secret': 'wowSoSecret',
-    'database': "mongodb://client:qwerty@ds042417.mlab.com:42417/arqsi20172018",
-    'IT1': 'http://arqsiit1.azurewebsites.net/api/',
-    'smtp2User': "arqsiIT2",
-    'smtp2Passwd': "Qwerty123!"
-};
